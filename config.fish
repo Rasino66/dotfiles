21e1779ee9c41d1139650cfbbecbb22f226cdf37
rasino66 <a.rasino66@gmail.com>
2019-03-26 12:04:07 +0900

    add dotfiles

---
+++ config.fish
@@ -0,0 +1,9 @@
+#if status --is-interactive
+#    set BASE16_SHELL "$HOME/.config/base16-shell/"
+#    source "$BASE16_SHELL/profile_helper.fish"
+#end
+
+set PATH ~/bin $PATH
+alias navi='navi.sh'
+alias docker-compose='docker-compose.exe'
+set DISPLAY localhost:0
